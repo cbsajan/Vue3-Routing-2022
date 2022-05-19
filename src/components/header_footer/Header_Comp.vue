d67905d492190fcbe69096ae56137e0bf9a8cba4
C B Sajan <sajan@ckhathri.com>
2022-05-19 20:17:03 +0530

    Linking

--- src/components/header_footer/Header_Comp.vue
+++ src/components/header_footer/Header_Comp.vue
@@ -6,9 +6,15 @@
             </a>
 
             <ul class="nav nav-pills">
-                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Articles</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link">Home</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/articles" class="nav-link">Articles</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/contact" class="nav-link">Contact</router-link>
+                </li>
             </ul>
         </header>
     </div>
@@ -22,3 +28,49 @@ body {
     font-family: 'Roboto', sans-serif;
 }
 </style>
+
+<!-- 
+    Another Way  
+
+    <template>
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <span class="fs-4">Beardit.com</span>
+            </a>
+
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                
+                        Another Way
+                        <router-link to="/" custom v-slot="{ isActive }">
+                        <a href="/" class="nav-link" :class="[isActive && 'active']">Home</a>
+                    </router-link>
+                   
+                </li>
+                <li class="nav-item">
+                     <router-link to="/articles" custom v-slot="{ isActive }">
+                        <a href="/articles" class="nav-link" :class="[isActive && 'active']">Articles</a>
+                    </router-link>
+                   
+                </li>
+                <li class="nav-item">
+                   <router-link to="/contact" custom v-slot="{ isActive }">
+                        <a href="/contact" class="nav-link" :class="[isActive && 'active']">Contact</a>
+                    </router-link>
+                    <router-link to="/contact" class="nav-link">Contact</router-link>
+                </li>
+            </ul>
+        </header>
+    </div>
+</template>
+
+
+<style>
+body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+}
+</style>
+ -->
