3596e7b66af1dbaaa60e27e9f5398664977b489e
C B Sajan <sajan@ckhathri.com>
2022-05-25 10:30:30 +0530

    Commited

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import Contact from './components/Contact/Index_Comp.vue';
 import Home from './components/Index_comp.vue';
 import NotFound from './components/404_Error_Comp.vue'
 import Notify from './components/notify_Comp.vue';
+import Login from './components/login_comp.vue';
 const app =  createApp(App);
 const theData = (route) => {
     console.log(route);
@@ -24,16 +25,38 @@ const routes = createRouter({
     routes:[
         { path:'/', component: Home },
         { path:'/articles', component: Articles },
+        { path:'/articles/:articleId', component: Article, props:theData },
+        // { path:'/articles', component: Articles, children:[
+        //     { path:':articleId', component: Article, props:theData }
+        // ] },
+        // { path:'/contact', redirect:'/' },
         { path:'/contact', components: {
             default: Contact,
             notify: Notify
         }, name:'contact'},
-        { path:'/article/:articleId', component: Article, props:theData },
+        { path:'/login', component:Login},
         { path:'/:notFound(.*)', component: NotFound }
     ],
     linkActiveClass:'active'
 });
 
+
+routes.beforeEach((to,from,next)=>{
+    const isAuth = false;
+
+    if(to.path === '/'){
+        next()
+    } else {
+        if(to.path !== '/login' && !isAuth) return next({path:'/login'});
+        else if(to.path === '/login' && isAuth) return next({path:'/'})
+        return next();
+    }
+});
+
+
+routes.afterEach(()=>{
+    console.log('after each')
+});
 app.component('app-header',Header);
 app.component('app-footer',Footer);
 app.use(routes)
